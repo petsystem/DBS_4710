ad0a36a7896fdb253c91cb72de17eefe9a6db0e5
braaxxad <braaxxad@gmail.com>
2017-04-18 17:11:17 -0400

    Add files via upload

---
+++ PETSYSTEM.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petsystem;
+
+import jdk.nashorn.internal.codegen.CompilerConstants;
+
+/**
+ *
+ * @author xumc
+ */
+public class PETSYSTEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        Login.main();
+        
+    
+    }
+   
+    
+}
